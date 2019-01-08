be55f81ea62b1d1aa5aa49bc2f5822e49bc46a59
Claudia Bringas Everett <claudiaeverett9@gmail.com>
2019-01-07 22:31:43 -0500

    Adding Files

--- models/Profile.js
+++ models/Profile.js
@@ -12,12 +12,6 @@ const ProfileSchema = new Schema({
     required: true,
     max: 40
   },
-  company: {
-    type: String
-  },
-  website: {
-    type: String
-  },
   location: {
     type: String
   },
@@ -32,84 +26,19 @@ const ProfileSchema = new Schema({
   bio: {
     type: String
   },
-  githubusername: {
-    type: String
-  },
-  experience: [
-    {
-      title: {
-        type: String,
-        required: true
-      },
-      company: {
-        type: String,
-        required: true
-      },
-      location: {
-        type: String
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
-  education: [
-    {
-      school: {
-        type: String,
-        required: true
-      },
-      degree: {
-        type: String,
-        required: true
-      },
-      fieldofstudy: {
-        type: String,
-        required: true
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
   social: {
-    youtube: {
-      type: String
-    },
-    twitter: {
-      type: String
-    },
-    facebook: {
-      type: String
-    },
-    linkedin: {
-      type: String
-    },
-    instagram: {
-      type: String
-    }
+      youtube: {
+        type: String
+      },
+      twitter: {
+        type: String
+      },
+      facebook: {
+        type: String
+      },
+      instagram: {
+        type: String
+      }
   },
   date: {
     type: Date,
--- validation/profile.js
+++ validation/profile.js
@@ -15,19 +15,12 @@ module.exports = function validateProfileInput(data) {
   if (Validator.isEmpty(data.handle)) {
     errors.handle = 'Profile handle is required';
   }
-
   if (Validator.isEmpty(data.status)) {
     errors.status = 'Status field is required';
   }
 
   if (Validator.isEmpty(data.skills)) {
     errors.skills = 'Skills field is required';
-  }
-
-  if (!isEmpty(data.website)) {
-    if (!Validator.isURL(data.website)) {
-      errors.website = 'Not a valid URL';
-    }
   }
 
   if (!isEmpty(data.youtube)) {
@@ -45,12 +38,6 @@ module.exports = function validateProfileInput(data) {
   if (!isEmpty(data.facebook)) {
     if (!Validator.isURL(data.facebook)) {
       errors.facebook = 'Not a valid URL';
-    }
-  }
-
-  if (!isEmpty(data.linkedin)) {
-    if (!Validator.isURL(data.linkedin)) {
-      errors.linkedin = 'Not a valid URL';
     }
   }
 
